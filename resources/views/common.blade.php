4181feaf2d564278971eb2ab172c98fe595daf55
IrisObsidian <pupiloftheocean@gmail.com>
2016-11-25 00:19:20 +0800

    add Carousel

--- resources/views/common.blade.php
+++ resources/views/common.blade.php
@@ -21,11 +21,11 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+            <a class="navbar-brand" href="{{'/'}}">{{config('app.name')}}</a>
         </div>
         <div class="collapse navbar-collapse" id="example-navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span>&nbsp;首页</a></li>
+                <li class="active"><a href="{{url('/')}}"><span class="glyphicon glyphicon-home"></span>&nbsp;首页</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-picture"></span>&nbsp;相册<b class="caret"></b></a>
                     <ul class="dropdown-menu">
@@ -45,8 +45,8 @@
     </div>
 </nav>
 {{--页面body--}}
-<div class="container-fluid" style="padding-left: 50px;padding-right: 50px;padding-top: 50px;">
-    @yield('content')
+<div class="container-fluid" style="padding-left: 1.7em;padding-right: 1.5em;padding-top: 50px;">
+    <div class="row">@yield('content')</div>
 </div>
 {{--在body闭合标签前引入js文件--}}
 <script src="{{asset('js/jquery.min.js')}}"></script>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,39 @@
 @extends('common')
 @section('content')
-    <h1>测试文本</h1>
+    <div class="col-sm-12">
+        <div class="col-sm-3">
+            <div id="myCarousel" class="carousel slide">
+                <!-- 轮播（Carousel）指标 -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+                <!-- 轮播（Carousel）项目 -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="/wp-content/uploads/2014/07/slide1.png" alt="First slide">
+                    </div>
+                    <div class="item">
+                        <img src="/wp-content/uploads/2014/07/slide2.png" alt="Second slide">
+                    </div>
+                    <div class="item">
+                        <img src="/wp-content/uploads/2014/07/slide3.png" alt="Third slide">
+                    </div>
+                </div>
+                <!-- 轮播（Carousel）导航 -->
+                <a class="carousel-control left" href="#myCarousel"
+                   data-slide="prev">&lsaquo;
+                </a>
+                <a class="carousel-control right" href="#myCarousel"
+                   data-slide="next">&rsaquo;
+                </a>
+            </div>
+        </div>
+        <div class="col-sm-9">
+            测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本
+        </div>
+    </div>
 @endsection
 @section('script')
 @endsection
